d720e6648dab6f2c75af7a0296bbb890141cca9c
Altynbek Usenbekov <altosh.mail@gmail.com>
2022-09-09 01:55:23 +0600

    fix user avatar url can be null

--- lib/features/user/domain/entities/user.dart
+++ lib/features/user/domain/entities/user.dart
@@ -2,15 +2,15 @@ import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
   final String uname;
-  final String avatarUrl;
+  final String? avatarUrl;
   final int publicRepos;
 
   const User({
     required this.uname,
-    required this.avatarUrl,
+    this.avatarUrl,
     required this.publicRepos,
   });
 
   @override
-  List<Object> get props => [uname, avatarUrl, publicRepos];
+  List<Object> get props => [uname, avatarUrl ?? '', publicRepos];
 }
